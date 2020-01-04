a718b9f2a804e19d1cb982df379970a65a5a142a
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-05 00:44:00 +0800

    #3: Convert repository calls to promises

--- src/api/user/UserRepository.js
+++ src/api/user/UserRepository.js
@@ -1,14 +1,16 @@
 const User = require('./UserModel');
 
-const getAllUsers = () => User.find({});
+const getAllUsers = () => User.find({}).exec();
 
-const getUserById = (id) => User.findById(id);
+const getUserById = (id) => User.findById(id).exec();
 
-const saveUser = (user) => User.create(user);
+const saveUser = (user) => User.create(user).exec();
 
-const updateUser = (_id, user) => User.findOneAndUpdate({ _id }, user, { useFindAndModify: false });
+const updateUser = (_id, user) => User.findOneAndUpdate(
+  { _id }, user, { useFindAndModify: false },
+).exec();
 
-const deleteUserById = (id) => User.findByIdAndDelete(id);
+const deleteUserById = (id) => User.findByIdAndDelete(id).exec();
 
 module.exports = {
   getAllUsers, getUserById, saveUser, deleteUserById, updateUser,
