de9071567a092e3e67a0336c900dea2b7803b428
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-11 22:02:31 +0800

    #5: Replace hard coded script on ci yml to its own script file

--- scripts/deploy-to-cloud.sh
+++ scripts/deploy-to-cloud.sh
@@ -1,12 +1,32 @@
-echo "I got executed"
-wget https://firebasestorage.googleapis.com/v0/b/coursely-stg.appspot.com/o/aws-ec2-keys%2Fcommuly-api.pem?alt=media -O commuly-api.pem
+# -p url to use to fetch ssh keys for EC2
+# -s file name of ssh keys
+# -u host url with username
+# -d directory name of the project to cd in
+# -b branch name to pull changes with
+# -e url link for the env file
+while getopts p:s:u:d:b:e: option
+do
+case "${option}"
+in
+p) SSH_KEYS_URL=${OPTARG};;
+s) SSH_KEYS=${OPTARG};;
+u) HOST_URL=${OPTARG};;
+d) PROJECT_DIRECTORY=${OPTARG};;
+b) BRANCH_NAME=${OPTARG};;
+e) ENV_URL=${OPTARG};;
+esac
+done
 
+echo "Fetching SSH keys for EC2 instance"
+wget ${SSH_KEYS_URL} -O ${SSH_KEYS}
+
+echo "Changing permission for ssh keys"
 chmod 400 commuly-api.pem
-
 pwd
 ls
 
-ssh -i "commuly-api.pem" ec2-user@ec2-54-225-55-219.compute-1.amazonaws.com -o StrictHostKeyChecking=no
+echo "Using SSH to access EC2 instance at ${HOST_URL}"
+echo "Deploying changes..."
+ssh -i ${SSH_KEYS} ${HOST_URL} -o StrictHostKeyChecking=no "cd ${PROJECT_DIRECTORY} && git pull origin ${BRANCH_NAME} && wget ${ENV_URL} -O .env && pm2 restart all"
 
-pwd
-ls
+echo "Done deploying changes to EC2 instance"
