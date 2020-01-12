ca7f9ab19f135bd2c568b68c78eb4db51ce9a933
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-12 13:40:32 +0800

    #4: Restructure directories

--- src/modules/interfaces/sms.js
+++ src/modules/interfaces/sms.js
@@ -1,5 +1,5 @@
 // TODO: Further testing
-const sendSNS = require('../implementations/awsSns');
+const sendSNS = require('../implementations/awsSNS');
 
 const sendSMS = (phoneNumber, message, sender) => {
   const messageData = { phoneNumber, message, sender };
