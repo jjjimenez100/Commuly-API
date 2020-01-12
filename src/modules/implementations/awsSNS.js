15d983935fc903ef007146fefd139a46080b2649
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-12 19:57:22 +0800

    #21: Add ability to send text messages to multiple recipients

--- src/modules/implementations/awsSNS.js
+++ src/modules/implementations/awsSNS.js
@@ -1,35 +1,45 @@
 const SNS = require('aws-sdk/clients/sns');
 
-const sendSNS = (messageData) => {
-  const {
-    message, sender, phoneNumber,
-  } = messageData;
-  return new Promise((resolve, reject) => {
-    const sns = new SNS();
-    const params = {
-      Message: message,
-      MessageAttributes: {
-        'AWS.SNS.SMS.SMSType': {
-          DataType: 'String',
-          StringValue: messageData.type || 'Transactional',
-        },
-        'AWS.SNS.SMS.SenderID': {
-          DataType: 'String',
-          StringValue: sender,
-        },
-      },
-      MessageStructure: 'string',
-      PhoneNumber: phoneNumber,
+class SNSTextMessage {
+  constructor(accessKeyId, secretAccessKey, region) {
+    const accessConfig = {
+      accessKeyId, secretAccessKey, region,
     };
 
-    sns.publish(params, (error, data) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
+    this.snsInstance = new SNS(accessConfig);
+  }
 
-module.exports = sendSNS;
+  sendSNS(messageData) {
+    const {
+      message, sender, phoneNumber,
+    } = messageData;
+
+    return new Promise((resolve, reject) => {
+      const params = {
+        Message: message,
+        MessageAttributes: {
+          'AWS.SNS.SMS.SMSType': {
+            DataType: 'String',
+            StringValue: messageData.type || 'Transactional',
+          },
+          'AWS.SNS.SMS.SenderID': {
+            DataType: 'String',
+            StringValue: sender,
+          },
+        },
+        MessageStructure: 'string',
+        PhoneNumber: phoneNumber,
+      };
+
+      this.snsInstance.publish(params, (error, data) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  }
+}
+
+module.exports = SNSTextMessage;
--- src/modules/interfaces/sms.js
+++ src/modules/interfaces/sms.js
@@ -1,9 +1,36 @@
 // TODO: Further testing
-const sendSNS = require('../implementations/awsSNS');
+const SMSWrapper = require('../implementations/awsSNS');
+const logger = require('../logger');
+const {
+  ACCESS_KEY_ID, SECRET_ACCESS_KEY, AWS_REGION,
+} = require('../../config/aws');
 
-const sendSMS = (phoneNumber, message, sender) => {
+const sms = new SMSWrapper(ACCESS_KEY_ID, SECRET_ACCESS_KEY, AWS_REGION);
+
+const sendSMS = async (phoneNumber, message, sender) => {
   const messageData = { phoneNumber, message, sender };
-  return sendSNS(messageData);
+  try {
+    await sms.sendSNS(messageData);
+    return true;
+  } catch (error) {
+    logger.error(`Failed to send SMS message ${error}`);
+    throw error;
+  }
 };
 
-module.exports = sendSMS;
+const sendSMSToMultipleRecipients = async (phoneNumbers, message, sender) => {
+  const smsQueue = phoneNumbers.map((phoneNumber) => {
+    const messageData = { phoneNumber, message, sender };
+    return sms.sendSNS(messageData);
+  });
+
+  try {
+    await Promise.all(smsQueue);
+    return true;
+  } catch (error) {
+    logger.error(`Failed to send SMS message to one of the recipients ${error}`);
+    throw error;
+  }
+};
+
+module.exports = { sendSMSToMultipleRecipients, sendSMS };
