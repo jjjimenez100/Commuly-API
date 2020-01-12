2bcf874d8b2320824558e927b31c72840e0e1b62
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-12 16:29:44 +0800

    #25: Update deployment script to fetch RSA keys for Cloudfront

--- scripts/deploy-to-cloud.sh
+++ scripts/deploy-to-cloud.sh
@@ -1,15 +1,17 @@
 # -p url to use to fetch ssh keys for EC2
 # -s file name of ssh keys
+# -c url to use to fetch rsa keys for Cloudfront
 # -u host url with username
 # -d directory name of the project to cd in
 # -b branch name to pull changes with
 # -e url link for the env file
-while getopts p:s:u:d:b:e: option
+while getopts p:s:c:u:d:b:e: option
 do
 case "${option}"
 in
 p) SSH_KEYS_URL=${OPTARG};;
 s) SSH_KEYS=${OPTARG};;
+c) CLOUDFRONT_URL=${OPTARG};;
 u) HOST_URL=${OPTARG};;
 d) PROJECT_DIRECTORY=${OPTARG};;
 b) BRANCH_NAME=${OPTARG};;
@@ -25,6 +27,6 @@ chmod 400 commuly-api.pem
 
 echo "Using SSH to access EC2 instance"
 echo "Deploying changes..."
-ssh -i ${SSH_KEYS} ${HOST_URL} -o StrictHostKeyChecking=no "cd ${PROJECT_DIRECTORY} && git pull origin ${BRANCH_NAME} && wget ${ENV_URL} -O .env && yarn install && pm2 restart all"
+ssh -i ${SSH_KEYS} ${HOST_URL} -o StrictHostKeyChecking=no "cd ${PROJECT_DIRECTORY} && git pull origin ${BRANCH_NAME} && wget ${ENV_URL} -O .env && wget ${CLOUDFRONT_URL} -O commuly-api-cloudfront-key.pem && yarn install && pm2 restart all"
 
 echo "Done deploying changes to EC2 instance"
