e185e7d314a7523b63657af0020bd1b9f8bcfc94
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-25 23:33:20 +0800

    #37: Remodel Team

---
+++ src/api/team/TeamModel.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+
+const { ObjectId } = Schema.Types;
+
+const threshold = {
+  type: {
+    threshold: Number,
+    notificationType: String,
+  },
+};
+
+const thresholds = {
+  thresholds: {
+    type: {
+      understood: {
+        ...threshold,
+      },
+      excited: {
+        ...threshold,
+      },
+      bored: {
+        ...threshold,
+      },
+      confused: {
+        ...threshold,
+      },
+    },
+  },
+};
+
+const teamSchema = new Schema({
+  name: String,
+  members: [ObjectId],
+  admins: [ObjectId],
+  pinnedCards: [ObjectId],
+  ...thresholds,
+}, { typePojoToMixed: false });
+
+const Team = model('Team', teamSchema);
+module.exports = Team;
