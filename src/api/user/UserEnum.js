c2dccf0758a4338bdeb0385d658ffc0cfe155550
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-01-31 02:30:37 +0800

    #37: Move TODO status enums under user

---
+++ src/api/user/UserEnum.js
@@ -0,0 +1,14 @@
+const DONE_STATUS = 'DONE';
+const STUCK_STATUS = 'STUCK';
+
+const TODO_STATUS = [
+  DONE_STATUS,
+  STUCK_STATUS,
+];
+
+module.exports = {
+  DONE_STATUS,
+  STUCK_STATUS,
+
+  TODO_STATUS,
+};
