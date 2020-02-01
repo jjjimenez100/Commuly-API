6fad358e7e8b2018fe14cbf103543be781666e7d
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-02 02:00:50 +0800

    #70: Add swagger-ui-express dependency

--- src/loaders/route.js
+++ src/loaders/route.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('../resources/swagger.json');
 
 const logger = require('../modules/logger');
 const errorHandler = require('../api/errorHandler');
@@ -14,6 +16,8 @@ const initRoutes = (app) => {
   logger.info('Routes');
   app.use(BASE_URL, router);
   app.use(errorHandler);
+  // for swagger documentation
+  app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
   logger.info(' \u2714 mounted');
 };
 
