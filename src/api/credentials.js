39d846cf2eee188069076ffc47ab64fe58646512
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-13 23:22:35 +0800

    #46: Add user role on generated JWT token

--- src/api/credentials.js
+++ src/api/credentials.js
@@ -1,8 +1,6 @@
 const crypto = require('crypto');
 const jwtSigner = require('jsonwebtoken');
-const jwt = require('express-jwt');
 const moment = require('moment-timezone');
-const passport = require('passport');
 
 const { JWT_PRIVATE_KEY } = require('../config/authentication');
 
@@ -15,62 +13,19 @@ const isPasswordValid = (password, salt, hash) => hashPassword(password, salt) =
 
 const generateSalt = () => crypto.randomBytes(16).toString('hex');
 
-const generateJWT = (userId, email) => {
-  const expirationDate = moment.tz().add(1, 'hour');
-  const tokenDetails = { userId, email, expirationDate };
+const generateJWT = (userId, email, role) => {
+  const expirationDate = moment.tz().add(1, 'hour').format();
+  const tokenDetails = {
+    userId, email, role, expirationDate,
+  };
+  const token = jwtSigner.sign(tokenDetails, JWT_PRIVATE_KEY);
 
-  return jwtSigner.sign(tokenDetails, JWT_PRIVATE_KEY);
+  return { token, expirationDate };
 };
-
-const getTokenFromHeaders = (request) => {
-  const { headers: { authorization } } = request;
-  const tokens = authorization.split(' ');
-
-  if (authorization && tokens[0] === 'Token') {
-    return tokens[1];
-  }
-
-  return null;
-};
-
-const authObj = {
-  secret: 'secret',
-  userProperty: 'payload',
-  getToken: getTokenFromHeaders,
-};
-
-const auth = {
-  required: jwt({
-    ...authObj,
-  }),
-  optional: jwt({
-    ...authObj,
-    credentialsRequired: false,
-  }),
-};
-
-const authenticate = () => new Promise((resolve, reject) => {
-  passport.authenticate(
-    'jwt',
-    { session: false },
-    (error, user) => {
-      if (error) {
-        reject(new Error(error));
-      } else if (!user) {
-        reject(new Error('Not authenticated'));
-      }
-      resolve(user);
-    },
-  );
-});
-
 
 module.exports = {
   hashPassword,
   isPasswordValid,
   generateJWT,
   generateSalt,
-
-  auth,
-  authenticate,
 };
