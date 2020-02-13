f2bbe7782286b91da865198c175e0dbd1b48f537
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-13 10:38:43 +0800

    #000: Test deployment with logs

--- src/loaders/variable.js
+++ src/loaders/variable.js
@@ -3,6 +3,8 @@ const logger = require('../modules/logger');
 
 const initVariablesOnExternalFiles = () => {
   logger.info('Environment Variables');
+  console.log('env value is: ', process.env.CLOUDFRONT_RSA_KEY);
+  console.log('env rsa key path value is: ', process.env.CLOUDFRONT_RSA_KEY_PATH);
   process.env.CLOUDFRONT_RSA_KEY = fileSystem.readFileSync(process.env.CLOUDFRONT_RSA_KEY_PATH);
   logger.info(' \u2714 loaded');
 };
