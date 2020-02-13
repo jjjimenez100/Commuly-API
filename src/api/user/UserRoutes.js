4324be58c8f50924995e97ba2af7bb596dbbe6f3
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-13 23:25:05 +0800

    #46: Add token checking on all USER ROUTES

--- src/api/user/UserRoutes.js
+++ src/api/user/UserRoutes.js
@@ -1,4 +1,13 @@
 const router = require('express').Router();
+
+const jwt = require('express-jwt');
+const { JWT_PRIVATE_KEY } = require('../../config/authentication');
+const { roleChecker } = require('../roleChecker');
+const {
+  ADMINISTRATOR_ROLE,
+  USER_ROLES,
+} = require('./UserEnum');
+
 const {
   getAllUsers,
   getUserById,
@@ -12,17 +21,18 @@ const {
 const { UserValidation, idValidation } = require('./UserValidation');
 const { validate } = require('../validation');
 
-router.get('/users', getAllUsers);
-router.get('/user/:id', idValidation, validate, getUserById);
-router.get('/user/:id/cards', idValidation, validate, getUserCards);
+router.get('/users', jwt({ secret: JWT_PRIVATE_KEY }), roleChecker([ADMINISTRATOR_ROLE]), getAllUsers);
+router.get('/user/:id', idValidation, validate, jwt({ secret: JWT_PRIVATE_KEY }), roleChecker([ADMINISTRATOR_ROLE]), getUserById);
+router.get('/user/:id/cards', idValidation, validate, jwt({ secret: JWT_PRIVATE_KEY }), roleChecker(USER_ROLES), getUserCards);
 
+// Public route for registration and login
 router.post('/users', UserValidation, validate, postUser);
 router.post('/login', loginUser);
 
-router.put('/user/:id', UserValidation, validate, updateUser);
+router.put('/user/:id', UserValidation, validate, jwt({ secret: JWT_PRIVATE_KEY }), roleChecker([ADMINISTRATOR_ROLE]), updateUser);
 
-router.patch('/user/:id/cards', patchUserCard);
+router.patch('/user/:id/cards', jwt({ secret: JWT_PRIVATE_KEY }), roleChecker([ADMINISTRATOR_ROLE]), patchUserCard);
 
-router.delete('/user/:id', idValidation, validate, deleteUser);
+router.delete('/user/:id', idValidation, validate, jwt({ secret: JWT_PRIVATE_KEY }), roleChecker([ADMINISTRATOR_ROLE]), deleteUser);
 
 module.exports = router;
