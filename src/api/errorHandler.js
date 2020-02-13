3f74868354cf4f83ffb47e7010925c441b8b1fe3
jjjimenez100 <jimenez.johnjoshua.jjj@gmail.com>
2020-02-13 23:25:36 +0800

    #46: Handle specific 401 errors on default error handler

--- src/api/errorHandler.js
+++ src/api/errorHandler.js
@@ -1,6 +1,21 @@
 const logger = require('../modules/logger');
 
 const errorHandler = (error, request, response, next) => {
+  const { name = '', code = '', message = '' } = error;
+  if (name === 'UnauthorizedError') {
+    if (code === 'credentials_required') {
+      return response.status(401).send({
+        message,
+      });
+    }
+
+    if (code === 'invalid_token') {
+      return response.status(401).send({
+        message: 'Token has been tampered',
+      });
+    }
+  }
+
   logger.error(`Server Error. ${error}`);
   if (response.headersSent) {
     return next(error);
